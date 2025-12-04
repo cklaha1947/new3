2ccf45f630685ab136324c23b62e0dc88ea5ad27
Chanchal Kumar Laha <cklaha1947@gmail.com>
2025-12-04 20:37:09 +0530

    2nd time added

--- index.php
+++ index.php
@@ -6,18 +6,16 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Chanchal Kumar Laha new</h1>
+<!-- http://ec2-18-60-227-33.ap-south-2.compute.amazonaws.com/myproject/  -->
+    <h1>Chanchal Kumar Laha new 1068</h1>
     <h1>Successfully run devops CI/CD Pipeline.</h1>
-<<<<<<< HEAD
     <img style="width: 300px;height:auto;" src="./img/img1.jpeg" alt=""> 
-=======
     <h1>Successfully run devops CI/CD Pipeline.</h1>
     <h1>Successfully run devops CI/CD Pipeline.</h1>
     <h1>Successfully run devops CI/CD Pipeline.</h1>
     <h1>Successfully run devops CI/CD Pipeline.</h1>
     <h1>Successfully run devops CI/CD Pipeline.</h1>
     <img style="width: 300px;height:auto;" src="./img/img1.jpeg" alt="">
->>>>>>> b295ca9 (Updated design and fixed bugmmms)
     <img style="width: 300px;height:auto;" src="./img/img1.jpeg" alt="">
     <img style="width: 300px;height:auto;" src="./img/img1.jpeg" alt="">
     <img style="width: 100vw;height:auto;" src="./img/img1.jpeg" alt="">
